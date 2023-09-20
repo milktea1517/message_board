5a2fb0d355e33dd5cda1ef6c51e6bba43c509869
ajie1517 <milktea8914@gmail.com>
2023-09-20 13:40:27 +0800

    update

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE message_board;
+
 CREATE TABLE `message_board`.`userdata` (`user_id` INT(11) NOT NULL AUTO_INCREMENT ,
 `nickname` VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL ,
 `username` VARCHAR(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL UNIQUE,
